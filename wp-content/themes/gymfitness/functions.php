5b11e7907c4925c94b4d4af2975304501b34e87e
juan-carlos-mamani-gutierrez <jiseroca65528@gmail.com>
2023-12-17 10:08:36 +0100

    creado la galeria

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -1,7 +1,7 @@
 <?php
 // TODO  Includes
- 
-require get_template_directory().'/includes/widgets.php';
+
+require get_template_directory() . '/includes/widgets.php';
 
 // TODO imagenes abilitar
 function gymfitness_setup()
@@ -26,8 +26,15 @@ add_action('init', 'gymfitness_menus');
 
 function gymfitness_scripts_styles()
 {
+    //TODO archivos css
+
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+    wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.4');
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
+
+    //TODO  archivos de js
+   
+    wp_enqueue_script('lightboxjs',get_template_directory_uri().'/js/lightbox.min.js',array('jquery'),'2.11.4',true);
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
