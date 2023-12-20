08eb15498f42ae581db622cfba248e08021e9870
juan-carlos-mamani-gutierrez <jiseroca65528@gmail.com>
2023-12-20 13:33:47 +0100

    creacion de pagina de contacto

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -6,18 +6,18 @@ require get_template_directory() . '/includes/widgets.php';
 // TODO imagenes abilitar
 function gymfitness_setup()
 {
-    //TODO imagenes
-    add_theme_support('post-thumbnails');
-    //TODO Titulo para seo
-    add_theme_support('title-tag');
+  //TODO imagenes
+  add_theme_support('post-thumbnails');
+  //TODO Titulo para seo
+  add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'gymfitness_setup');
 // TODO  menu 
 function gymfitness_menus()
 {
-    register_nav_menus(array(
-        'menu-principal' => __('Menu Principal', 'gymfitness')
-    ));
+  register_nav_menus(array(
+    'menu-principal' => __('Menu Principal', 'gymfitness')
+  ));
 }
 
 add_action('init', 'gymfitness_menus');
@@ -26,15 +26,15 @@ add_action('init', 'gymfitness_menus');
 
 function gymfitness_scripts_styles()
 {
-    //TODO archivos css
+  //TODO archivos css
 
-    wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
-    wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.4');
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
+  wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+  wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.4');
+  wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
 
-    //TODO  archivos de js
-   
-    wp_enqueue_script('lightboxjs',get_template_directory_uri().'/js/lightbox.min.js',array('jquery'),'2.11.4',true);
+  //TODO  archivos de js
+
+  wp_enqueue_script('lightboxjs', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '2.11.4', true);
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
@@ -42,23 +42,42 @@ add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
 //TODO definir zona de widgets
 function gymfitness_widgets()
 {
-    register_sidebar(array(
-        'name' => 'Sidebar 1',
-        'id' => 'sidebar_1',
-        'before_widget' => '<div class="widget">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3 class="text-center text-primary">',
-        'after_title' => '</h3>'
+  register_sidebar(array(
+    'name' => 'Sidebar 1',
+    'id' => 'sidebar_1',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="text-center text-primary">',
+    'after_title' => '</h3>'
 
-    ));
-    register_sidebar(array(
-        'name' => 'Sidebar 2',
-        'id' => 'sidebar_2',
-        'before_widget' => '<div class="widget">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3 class="text-center text-primary">',
-        'after_title' => '</h3>'
+  ));
+  register_sidebar(array(
+    'name' => 'Sidebar 2',
+    'id' => 'sidebar_2',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="text-center text-primary">',
+    'after_title' => '</h3>'
 
-    ));
+  ));
 }
 add_action('widgets_init', 'gymfitness_widgets');
+
+//Crear Shortcode
+
+function gymfitness_ubicacion_shortcode()
+{
+
+?>
+  <div class="mapa">
+    <?php
+    if (is_page('contacto')) {
+      the_field('ubicacion');
+    }
+    ?>
+  </div>
+  <h2 class="text-center text-primary">Formulario de Contacto</h2>
+<?php
+  echo do_shortcode('[contact-form-7 id="d1f0583" title="Contact form 1"]');
+}
+add_shortcode('gymfitness_ubicacion', 'gymfitness_ubicacion_shortcode');
--- wp-content/themes/gymfitness/template-parts/pagina.php
+++ wp-content/themes/gymfitness/template-parts/pagina.php
@@ -2,15 +2,16 @@
 // TODO  loop de contenido
 
 while (have_posts()) : the_post();
-    the_title('<h1 class="text-center text-primary">', '</h1>');
+  the_title('<h1 class="text-center text-primary">', '</h1>');
 
-    //TODO si quieres anadir una clase
-    /* 
+  //TODO si quieres anadir una clase
+  /* 
  the_post_thumbnail('full', array('class' => 'imagen-destacada'));
   */
-    if (has_post_thumbnail()) {
-        the_post_thumbnail('full', array('class' => 'imagen-destacada'));
-    }
-  
-    the_content();
+  if (has_post_thumbnail()) {
+    the_post_thumbnail('full', array('class' => 'imagen-destacada'));
+  }
+
+
+  the_content();
 endwhile;
